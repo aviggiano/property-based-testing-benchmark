e821e1be93c4e5c2d23ef6819ffd3219a5995249
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-10-13 14:34:46 -0300

    Rename log2 to log_2

--- projects/abdk-math-64x64/test/ABDKMath64x64Log_2Properties.t.sol
+++ projects/abdk-math-64x64/test/ABDKMath64x64Log_2Properties.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "./ABDKMath64x64Setup.t.sol";
 
 contract ABDKMath64x64Log_2Properties is ABDKMath64x64Setup {
-    // log2(x * y) = log2(x) + log2(y)
+    // log_2(x * y) = log_2(x) + log2(y)
     function test_log_2_distributive_mul(int128 x, int128 y) public {
         // Ensure we have enough significant digits for the result to be meaningful
         try abdk.significant_bits_after_mul(x, y) returns (uint256 s__x__y) {
@@ -12,11 +12,11 @@ contract ABDKMath64x64Log_2Properties is ABDKMath64x64Setup {
             try abdk.log_2(x) returns (int128 log_2_x) {
                 try abdk.log_2(y) returns (int128 log_2_y) {
                     try abdk.mul(x, y) returns (int128 xy) {
-                        try abdk.log_2(xy) returns (int128 log2_xy) {
+                        try abdk.log_2(xy) returns (int128 log_2_xy) {
                             try abdk.add(log_2_x, log_2_y) returns (
-                                int128 log2_x_log2_y
+                                int128 log_2_x_log_2_y
                             ) {
-                                try abdk.abs(log2_x_log2_y) returns (
+                                try abdk.abs(log_2_x_log_2_y) returns (
                                     int128 abs_log_2_x_log_2_y
                                 ) {
                                     // The maximum loss of precision is given by the formula:
@@ -27,8 +27,8 @@ contract ABDKMath64x64Log_2Properties is ABDKMath64x64Setup {
 
                                     try
                                         abdk.equal_within_precision(
-                                            log2_xy,
-                                            log2_x_log2_y,
+                                            log_2_xy,
+                                            log_2_x_log_2_y,
                                             loss
                                         )
                                     returns (bool equal) {
@@ -43,13 +43,13 @@ contract ABDKMath64x64Log_2Properties is ABDKMath64x64Setup {
         } catch {}
     }
 
-    // log2(x ** y) = y * log2(x)
+    // log_2(x ** y) = y * log_2(x)
     function test_log_2_power(int128 x, uint256 y) public {
         try abdk.log_2(x) returns (int128 log_2_x) {
             try abdk.pow(x, y) returns (int128 x_y) {
-                try abdk.log_2(x_y) returns (int128 log2_x_y) {
-                    try abdk.mulu(log_2_x, y) returns (uint256 y_log2_x) {
-                        assertEq(y_log2_x, abdk.toUInt(log2_x_y));
+                try abdk.log_2(x_y) returns (int128 log_2_x_y) {
+                    try abdk.mulu(log_2_x, y) returns (uint256 y_log_2_x) {
+                        assertEq(y_log_2_x, abdk.toUInt(log_2_x_y));
                     } catch {}
                 } catch {}
             } catch {}
