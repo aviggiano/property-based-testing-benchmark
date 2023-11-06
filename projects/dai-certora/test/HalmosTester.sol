48aec41bad46c950912db9c45616ffaa978de06d
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-11-06 16:57:59 -0300

    Assume calls never revert

--- projects/dai-certora/test/HalmosTester.sol
+++ projects/dai-certora/test/HalmosTester.sol
@@ -35,17 +35,25 @@ contract HalmosTester is TargetFunctions, SymTest, HalmosAsserts {
 
     function assumeSuccessfulCall(bytes4 selector) internal {
         if (selector == minivat.init.selector) {
-            try this.init() {} catch {}
+            try this.init() {} catch {
+                vm.assume(false);
+            }
         } else if (selector == minivat.move.selector) {
             address dst = svm.createAddress("dst");
             int256 wad = svm.createInt256("wad");
-            try this.move(dst, wad) {} catch {}
+            try this.move(dst, wad) {} catch {
+                vm.assume(false);
+            }
         } else if (selector == minivat.frob.selector) {
             int256 dart = svm.createInt256("dart");
-            try this.frob(dart) {} catch {}
+            try this.frob(dart) {} catch {
+                vm.assume(false);
+            }
         } else if (selector == minivat.fold.selector) {
             int256 delta = svm.createInt256("delta");
-            try this.fold(delta) {} catch {}
+            try this.fold(delta) {} catch {
+                vm.assume(false);
+            }
         } else {
             // selector bytes4(0) means "skip this call"
         }
